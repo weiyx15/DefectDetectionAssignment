e592a4334871de21e766efaeb157331e12389519
weiyx15 <weiyx15@outlook.com>
2019-12-02 09:41:59 +0800

    params from ziqiang

--- classifiers.py
+++ classifiers.py
@@ -27,6 +27,25 @@ def lgbm(trainX, trainY, testX, testY) -> typing.Tuple[float]:
         'max_depth': 4,
         'objective': 'binary',
     }
+
+    # params from Ziqiang
+    # params = {
+    #     'task': 'train',
+    #     'boosting_type': 'gbdt',
+    #     'objective': 'regression',
+    #     'metric': {'l2', 'auc'},
+    #     'num_leaves': 10,
+    #     'learning_rate': 0.1,
+    #     'feature_fraction': 0.9,
+    #     'bagging_fraction': 0.8,
+    #     'bagging_freq': 5,
+    #     'verbose': 0,
+    #     'is_unbalance': 'true',
+    #     # avoid too few sample error
+    #     'min_data': 1,
+    #     'min_data_in_bin': 1
+    # }
+
     clf = lgb.train(params, train_data)
     predY = clf.predict(testX)
     predY = [1 if pred > .5 else 0 for pred in predY]       # binarize
